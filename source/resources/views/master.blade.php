8063ae13d6fcec9ce6a4bbffadf37b14f3acd020
DinhTrong <trandinhtrongit@gmail.com>
2015-12-22 10:59:05 +0700

    fix asset path

--- source/resources/views/master.blade.php
+++ source/resources/views/master.blade.php
@@ -4,9 +4,9 @@
         <title>Main page</title>
         <meta charset="UTF-8">
         <meta name="viewport" content="width=device-width, initial-scale=1.0">
-        <link rel="stylesheet" media="screen" href="{{ asset('html/css/bootstrap.min.css') }}">
-        <link rel="stylesheet/less" type="text/css" href="{{ asset('html/css/style.less') }}" />
-        <script type="text/javascript" src="{{asset('html/js/less.min.js')}}"></script>
+        <link rel="stylesheet" media="screen" href="{{ asset('assets/js/bootstrap.min.css') }}">
+        <link rel="stylesheet/less" type="text/css" href="{{ asset('assets/css/style.less') }}" />
+        <script type="text/javascript" src="{{asset('assets/js/less.min.js')}}"></script>
         <script type="text/javascript">
             var baseUrl = "{{asset('/')}}";
         </script>
@@ -18,8 +18,8 @@
         <div class="container">
             @yield('content')
         </div>
-        <script src="{{ asset('html/js/jquery-1.11.3.js') }}"></script>
-        <script src="{{ asset('html/js/bootstrap.min.js') }}"></script>
+        <script src="{{ asset('assets/js/jquery-1.11.3.js') }}"></script>
+        <script src="{{ asset('assets/js/bootstrap.min.js') }}"></script>
         <script src="{{ asset('assets/js/app.js') }}"></script>
         @yield('js')
     </body>
