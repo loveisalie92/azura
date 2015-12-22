2e8db7e6fe3b8ac58ffbfb28fce15c36b0fa019b
DinhTrong <trandinhtrongit@gmail.com>
2015-12-22 10:57:20 +0700

    add ajax notification

---
+++ source/resources/views/_partials/notifications.blade.php
@@ -0,0 +1,14 @@
+<div id="ajax-notification"></div>
+<script type="text/template" id="ajax-notification-success-template">
+    <div class="alert alert-success fade in" id="[ID]">
+    <a title="close" aria-label="close" data-dismiss="alert" class="close" href="#">×</a>
+    [MESSAGE]
+    </div>
+</script>
+<script type="text/template" id="ajax-notification-error-template">
+    <div class="alert alert-danger fade in" id="[ID]">
+    <a title="close" aria-label="close" data-dismiss="alert" class="close" href="#">×</a>
+    [MESSAGE]
+    </div>
+</script>
+
--- source/resources/views/master.blade.php
+++ source/resources/views/master.blade.php
@@ -5,18 +5,22 @@
         <meta charset="UTF-8">
         <meta name="viewport" content="width=device-width, initial-scale=1.0">
         <link rel="stylesheet" media="screen" href="{{ asset('html/css/bootstrap.min.css') }}">
-	    <link rel="stylesheet/less" type="text/css" href="{{ asset('html/css/style.less') }}" />
+        <link rel="stylesheet/less" type="text/css" href="{{ asset('html/css/style.less') }}" />
         <script type="text/javascript" src="{{asset('html/js/less.min.js')}}"></script>
+        <script type="text/javascript">
+            var baseUrl = "{{asset('/')}}";
+        </script>
         @yield('css')
     </head>
 
     <body>
+        @include('_partials.notifications')
         <div class="container">
             @yield('content')
         </div>
-
         <script src="{{ asset('html/js/jquery-1.11.3.js') }}"></script>
-    	<script src="{{ asset('html/js/bootstrap.min.js') }}"></script>
+        <script src="{{ asset('html/js/bootstrap.min.js') }}"></script>
+        <script src="{{ asset('assets/js/app.js') }}"></script>
         @yield('js')
     </body>
 </html>
